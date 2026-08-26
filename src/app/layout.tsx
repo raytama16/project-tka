42f40c3f13fc6c4393e60e023d079d772bf13add
raya-pratama <gpraya257@gmail.com>
2026-08-26 18:45:54 +0700

    perbaikan logo web lagi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -82,7 +82,7 @@ export default function RootLayout({
     <html lang="id" className="scroll-smooth">
       <head>
         {/* Favicon & Icons */}
-        <link rel="icon" href="/logo.svg" sizes="any" />
+        <link rel="icon" href="/logo.svg" sizes="any" type="image/x-icon" />
         
         {/* CDN KaTeX CSS untuk merender rumus matematika/sains dengan rapi */}
         <link 
